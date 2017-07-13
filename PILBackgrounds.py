90455c2b91a250c05a9be7ef67d76af93614015a
Fish2 <Fish2@users.noreply.github.com>
2017-07-13 21:47:31 +0100

    posable fix

--- PILBackgrounds.py
+++ PILBackgrounds.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 
-def generate(PMS_uuid, url, authtoken, resolution):
+def generate(PMS_uuid, url, authtoken, resolution, blurRadius):
     cachepath = sys.path[0]+"/assets/fanartcache"
     stylepath = sys.path[0]+"/assets/thumbnails"
 
@@ -29,10 +29,10 @@ def generate(PMS_uuid, url, authtoken, resolution):
     if id:
         # assumes URL in format "/library/metadata/<ratingKey>/art/fileId>"
         id = id.groupdict()
-        cachefile = urllib.quote_plus(PMS_uuid +"_"+ id['ratingKey'] +"_"+ id['fileId'] +"_"+ resolution) + ".jpg"
+        cachefile = urllib.quote_plus(PMS_uuid +"_"+ id['ratingKey'] +"_"+ id['fileId'] +"_"+ resolution +"_"+ blurRadius) + ".jpg"
     else:
         fileid = posixpath.basename(urlparse.urlparse(url).path)
-        cachefile = urllib.quote_plus(PMS_uuid +"_"+ fileid +"_"+ resolution) + ".jpg"  # quote: just to make sure...
+        cachefile = urllib.quote_plus(PMS_uuid +"_"+ fileid +"_"+ resolution +"_"+ blurRadius) + ".jpg"  # quote: just to make sure...
 
     # Already created?
     dprint(__name__, 1, 'Check for Cachefile.')  # Debug
@@ -60,15 +60,20 @@ def generate(PMS_uuid, url, authtoken, resolution):
         dprint(__name__, 0, 'IOError: {0} // url: {1}', str(e), url)
         return "/thumbnails/Background_blank_" + resolution + ".jpg"
 
+    blurRadius = int(blurRadius)
+
     # Get gradient template
     dprint(__name__, 1, 'Merging Layers.')  # Debug
     if resolution == '1080':
         width = 1920
         height = 1080
+        blurRegion = (0, 514, 1920, 1080)
         layer = Image.open(stylepath + "/gradient_1080.png")
     else:
         width = 1280
         height = 720
+        blurRegion = (0, 342, 1280, 720)
+        blurRadius = int(blurRadius / 1.5)
         layer = Image.open(stylepath + "/gradient_720.png")
 
     try:
@@ -80,6 +85,7 @@ def generate(PMS_uuid, url, authtoken, resolution):
         if bgHeight != height:
             background = background.resize((width, height), Image.ANTIALIAS)
             dprint(__name__,1 , "Resizing background")
+
 
         # Save to Cache
         background.save(cachepath+"/"+cachefile)
